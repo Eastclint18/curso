f792a307387041ee924bb58b9d95829e43c26ba5
Eastclint18 <igorivas18@gmail.com>
2022-11-29 02:13:36 -0300

    updated

--- script.js
+++ script.js
@@ -3,28 +3,27 @@ const materias = ["Historia ", "Geografia", "Matematicas", "Programacion"];
 
 const nombre = prompt("Ingresa tu nombre");
 const notas = [];
+    
 
 
     for (let i = 0 ; i< materias.length; i++){
-       let nota = parseInt(prompt(`Cuanto sacaste en ${materias[i]}`));
-       if ( nota > 10 ){ alert("Ingresa un numero del 1 al 10")}
-       else{
+       let nota = parseInt(prompt(`Cuanto sacaste del 0 a 10 en ${materias[i]}`));
+       
         notas.push(nota)}
-
-    }
+    
  
 
 let total=0;
 for(let i of notas) total+=i;
 let promedio = total / materias.length;
     if (promedio === 10 ){
-    document.write(`Buen trabajo ${nombre} tu promedio es ${promedio} y puedes estudiar lo que quieras`)
+    alert(`Buen trabajo ${nombre} tu promedio es ${promedio} y puedes estudiar lo que quieras`)
     } 
     else if (promedio >5 && promedio <10 ){
-        document.write(`Tu promedio es ${promedio} y puedes estudiar casi cualquier cosa`)
+        alert(`Tu promedio es ${promedio} y puedes estudiar casi cualquier cosa`)
 
         }else {
-            document.write(`tu promedio es ${promedio} estudia mas`)
+            alert(`tu promedio es ${promedio} estudia mas`)
         }
     
 
